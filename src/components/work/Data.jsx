3d54757c1fa5dc38c12bd3b78cd268f8690421a9
Saurabh Gurde <saurabhgurde@gmail.com>
2025-01-27 15:53:28 +0530

    minor changes

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,78 +1,101 @@
-import Work11 from "../../assets/work1.jpg";
-import Work22 from "../../assets/work2.jpg";
-import Work33 from "../../assets/work3.jpg";
-import Work44 from "../../assets/work4.jpg";
-import Work55 from "../../assets/Work5.png";
-import Work66 from "../../assets/Work6.png";
-import Work77 from "../../assets/Work7.png";
-import Work88 from "../../assets/Work8.png";
-import Work10 from "../../assets/Work10.png"
+import Work1 from "../../assets/work1.jpg";
+import Work2 from "../../assets/work2.jpg";
+import Work3 from "../../assets/work3.jpg";
+import Work4 from "../../assets/work4.jpg";
+import Work5 from "../../assets/Work5.png";
+import Work6 from "../../assets/Work6.png";
+import Work7 from "../../assets/Work7.png";
+import Work8 from "../../assets/Work8.png";
+import Work10 from "../../assets/Work10.png";
+import Work11 from "../../assets/work11.png";
+import Work12 from "../../assets/work12.png";
+import Work13 from "../../assets/work13.png";
 
 export const projectsData = [
   {
-    id: 1,
-    image: Work88,
-    title: "Threads-Clone",
+    id: 10,
+    image: Work11,
+    title: "Gofood delivery app",
     category: "Full-Stack",
-    link: "https://threads-clone-9hw7.vercel.app/sign-up",
+    link: "https://gofood.saurabhgurde.life",
   },
   {
-      id: 2,
-      image: Work77,
-      title: "Promptopia",
-      category: "Full-Stack",
-      link: "https://promptopia-balrammel.vercel.app",
-    },
+    id: 11,
+    image: Work12,
+    title: "Ecomvision dashboard",
+    category: "Full-Stack",
+    link: "https://ecomvision.saurabhgurde.life",
+  },
   {
-      id: 3,
-      image: Work10,
-      title: "iPhone 15",
-      category: "Frontend",
-      link: "https://appleanimation.vercel.app",
-    },
+    id: 12,
+    image: Work13,
+    title: "Ecomvision dashboard",
+    category: "Full-Stack",
+    link: "https://ecommerce.saurabhgurde.life",
+  },
+  // {
+  //   id: 1,
+  //   image: Work8,
+  //   title: "Threads-Clone",
+  //   category: "Full-Stack",
+  //   link: "https://threads-clone-9hw7.vercel.app/sign-up",
+  // },
+  {
+    id: 2,
+    image: Work7,
+    title: "Promptopia",
+    category: "Full-Stack",
+    link: "https://promptopia-balrammel.vercel.app",
+  },
+  {
+    id: 3,
+    image: Work10,
+    title: "iPhone 15",
+    category: "Frontend",
+    link: "https://appleanimation.vercel.app",
+  },
   {
     id: 4,
-    image: Work66,
+    image: Work6,
     title: "Customer_Operator",
     category: "Full-Stack",
     link: "https://drive.google.com/file/d/1p2-m8XkCQWLwkj4fK9I0Bkxk7ikZnaZU/view?usp=drivesdk",
   },
   {
     id: 5,
-    image: Work11,
+    image: Work1,
     title: "Tasky Applovation",
     category: "Frontend",
     link: "https://taskyapplication.netlify.app/",
   },
   {
     id: 6,
-    image: Work22,
+    image: Work2,
     title: "Notes app",
     category: "Frontend",
     link: "https://notes-appl-mobile.netlify.app/",
   },
   {
     id: 7,
-    image: Work33,
+    image: Work3,
     title: "Blog web",
     category: "Frontend",
     link: "https://bookblogs.netlify.app/",
   },
   {
     id: 8,
-    image: Work44,
+    image: Work4,
     title: "Calculator",
     category: "Frontend",
     link: "https://calculatordarklighttheme.netlify.app/",
   },
   {
     id: 9,
-    image: Work55,
+    image: Work5,
     title: "Zomato landing page",
     category: "Frontend",
     link: "https://zomatooo-clone.netlify.app",
   },
-    
 ];
 
 export const projectsNav = [
